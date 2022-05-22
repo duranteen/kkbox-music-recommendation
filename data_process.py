7f8955e10f9a692a2c4beabc7653094e60e74d4d
duranteen <durantin@163.com>
2022-05-22 20:56:02 +0800

    2022 5 22

--- data_process.py
+++ data_process.py
@@ -96,7 +96,8 @@ songs_data['third_genre_id'] = genreids_split[:, 2]
 songs_data['fourth_genre_id'] = genreids_split[:, 3]
 # label encoding
 genre_encoder = LabelEncoder()
-genre_encoder.fit(songs_data['first_genre_id'].append(songs_data['second_genre_id']).append(songs_data['third_genre_id']))
+genre_encoder.fit(
+    songs_data['first_genre_id'].append(songs_data['second_genre_id']).append(songs_data['third_genre_id']))
 songs_data['first_genre_id'] = genre_encoder.transform(songs_data['first_genre_id'])
 songs_data['second_genre_id'] = genre_encoder.transform(songs_data['second_genre_id'])
 songs_data['third_genre_id'] = genre_encoder.transform(songs_data['third_genre_id'])
@@ -159,10 +160,85 @@ songs_data['composer_cnt'] = songs_data['composer'].apply(lyricist_or_composer_c
 songs_data['first_lyricist_name'] = songs_data['lyricist'].apply(get_first_lyricist_or_composer)
 songs_data['first_composer_name'] = songs_data['composer'].apply(get_first_lyricist_or_composer)
 # label encoding
-columns = ['first_artist_name','first_lyricist_name','first_composer_name']
+columns = ['first_artist_name', 'first_lyricist_name', 'first_composer_name']
 for col in columns:
     print(col)
     encoder = LabelEncoder()
     encoder.fit(songs_data[col].fillna('nan'))
     songs_data[col] = encoder.transform(songs_data[col].fillna('nan'))
 
+# is featured
+songs_data['is_featured'] = songs_data['artist_name'].apply(lambda x: 1 if ' feat' in str(x) else 0).astype(np.int8)
+# language
+songs_data['language'] = songs_data['language'].fillna(-1)
+songs_data.drop(['genre_ids', 'artist_name', 'lyricist', 'composer'], axis=1, inplace=True)
+# age
+members_data['bd'] = members_data['bd'].apply(lambda x: np.nan if x < 0 or x >= 80 else x)
+
+# merge songs_data and song_extinfo_data
+song = pd.DataFrame({'song_id': range(max(train_data.song_id.max(), test_data.song_id.max()) + 1)})
+song = song.merge(songs_data, on='song_id', how='left')
+song = song.merge(song_extinfo_data, on='song_id', how='right')
+song_columns = ['language', 'first_genre_id', 'second_genre_id', 'third_genre_id', 'first_artist_name',
+                'first_lyricist_name', 'first_composer_name']
+for col in song_columns:
+    print(col)
+    col_song_cnt = song.groupby(by=col)['song_id'].count().to_dict()
+    song[col + '_song_cnt'] = song[col].apply(lambda x: col_song_cnt[x] if not np.isnan(x) else np.nan)
+
+data = train_data[['msno', 'song_id']].append(test_data[['msno', 'song_id']])
+msno_rec_cnt = data.groupby(by='msno')['song_id'].count().to_dict()
+members_data['msno_rec_cnt'] = members_data['msno'].apply(lambda x: msno_rec_cnt[x])
+data = data.merge(song, on='song_id', how='left')
+
+song_columns = ['song_id', 'language', 'first_genre_id', 'second_genre_id', 'third_genre_id', 'first_artist_name',
+                'first_lyricist_name', 'first_composer_name']
+# number of user feature
+for col in song_columns:
+    print(col)
+    col_rec_cnt = data.groupby(by=col)['msno'].count().to_dict()
+    song[col + '_rec_cnt'] = song[col].apply(lambda x: col_rec_cnt[x] if not np.isnan(x) else np.nan)
+
+# source_system_tab, source_system_screen_name, source_type: prob feature
+cols = ['source_system_tab', 'source_screen_name', 'source_type']
+concat = train_data.drop('target', axis=1).append(test_data.drop('id', axis=1))
+msno_rec_cnt = data.groupby(by='msno')['song_id'].count().to_dict()
+train_data['msno_rec_cnt'] = train_data['msno'].apply(lambda x: msno_rec_cnt[x])
+test_data['msno_rec_cnt'] = test_data['msno'].apply(lambda x: msno_rec_cnt[x])
+for col in cols:
+    print(col)
+    tmp = concat.groupby(['msno', col])['song_id'].agg(
+        [('msno_' + col + '_cnt', 'count')]).reset_index()  # 出现次数 & 出现次数占比
+    train_data = train_data.merge(tmp, on=['msno', col], how='left')
+    train_data['msno_' + col + '_prob'] = train_data['msno_' + col + '_cnt'] * 1.0 / train_data['msno_rec_cnt']
+
+    test_data = test_data.merge(tmp, on=['msno', col], how='left')
+    test_data['msno_' + col + '_prob'] = test_data['msno_' + col + '_cnt'] * 1.0 / test_data['msno_rec_cnt']
+
+train_data.drop('msno_rec_cnt', axis=1, inplace=True)
+test_data.drop('msno_rec_cnt', axis=1, inplace=True)
+# isrc
+isrc = song['isrc']
+song['cc'] = isrc.str.slice(0, 2)
+song['xxx'] = isrc.str.slice(2, 5)
+song['yy'] = isrc.str.slice(5, 7).astype(float)
+song['yy'] = song['yy'].apply(lambda x: 2000 + x if x < 18 else 1900 + x)
+song['cc'] = LabelEncoder().fit_transform(song['cc'].fillna('nan'))
+song['xxx'] = LabelEncoder().fit_transform(song['xxx'].fillna('nan'))
+song['irsc_missing'] = (song['cc'] == 0) * 1.0
+# irsc count
+columns = ['cc', 'xxx', 'yy']
+for col in columns:
+    print(col)
+    song_ccxxxyy_cnt = song.groupby(by=col)['song_id'].count().to_dict()
+    song_ccxxxyy_cnt[0] = None
+    song[col + '_song_cnt'] = song[col].apply(lambda x: song_ccxxxyy_cnt[x] if not np.isnan(x) else None)
+data = train_data[['msno', 'song_id']].append(test_data[['msno', 'song_id']])
+data = data.merge(song, on='song_id', how='left')
+columns = ['cc', 'xxx', 'yy']
+for col in columns:
+    print(col)
+    song_ccxxxyy_cnt = data.groupby(by=col)['song_id'].count().to_dict()
+    song_ccxxxyy_cnt[0] = None
+    song[col + '_rec_cnt'] = song[col].apply(lambda x: song_ccxxxyy_cnt[x] if not np.isnan(x) else None)
+song.drop(['name', 'isrc'], axis=1, inplace=True)
