b4f4988de74ff48c79d8b493f84779f3c73e045a
duranteen <durantin@163.com>
2022-06-06 20:31:18 +0800

    2022 5 22

--- model/net.py
+++ model/net.py
@@ -7,7 +7,7 @@ from model.gnn import GNN
 class Net(nn.Module):
     def __init__(self, user_input_dim, item_input_dim, hidden_dim,
                  embedding_dim, edge_dim, dropout=0.,
-                 activation=F.sigmoid, use_edge_feature=True):
+                 activation=torch.sigmoid, use_edge_feature=True):
         super(Net, self).__init__()
         self.dropout = nn.Dropout(dropout)
         self.activation = activation
@@ -27,7 +27,7 @@ class Net(nn.Module):
 
         feat = torch.cat((user_proj_feat, item_proj_feat), 0)
         if feat.shape[0] < adjacency.shape[0]:
-            feat = torch.cat((feat, torch.zeros((adjacency.shape[0]-feat.shape[0], feat.shape[1]), dtype=torch.float)), 1)
+            feat = torch.cat((feat, torch.zeros((adjacency.shape[0]-feat.shape[0], feat.shape[1]), dtype=torch.float)), 0)
 
         embd_user = self.gnn_user(adjacency, feat)
         embd_item = self.gnn_item(adjacency, feat)
